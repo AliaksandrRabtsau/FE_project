2cbb626f763346c5c0ac436a293b579996bd4f4d
rabtsau_aliaksandr <rabtsau.aliaksandr@gmail.com>
2018-11-03 16:08:12 +0300

    ! add third task (updated file)

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,30 +1,8 @@
-import {Component, OnInit} from '@angular/core';
-import {AuthService} from './shared/services/auth.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>'
 })
-export class AppComponent implements OnInit {
-  constructor(private auth: AuthService) {
-  }
 
-  ngOnInit() {
-    const potentialToken = localStorage.getItem('auth-token')
-    if (potentialToken !== null) {
-      this.auth.setToken(potentialToken)
-    }
-  }
-}
-
-
-// import { Component } from '@angular/core';
-//
-// @Component({
-//   selector: 'app-root',
-//   templateUrl: './app.component.html',
-//   styleUrls: ['./app.component.css']
-// })
-// export class AppComponent {
-//   title = 'my-app';
-// }
+export class AppComponent { }
